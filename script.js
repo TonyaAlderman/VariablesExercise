3c63edbc3d33f0ea7e1296af40bcf5dfb91cd6af
TonyaAlderman <talderm99@gmail.com>
2021-09-21 20:23:17 -0500

    finished VariablesExercise

---
+++ script.js
@@ -0,0 +1,13 @@
+// const for unchanging values
+let num = 20
+num = 15
+num += 7
+num--
+num = "this is not a number"
+let blank
+console.log(blank);
+blank = null
+const quarter=0.25, dime=0.10, nickel=0.05, penny=0.01
+console.log(quarter, dime, nickel, penny);
+let letters = "qwertyuiop";
+console.log(letters[9]);
